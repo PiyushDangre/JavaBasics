f529886eac175ff39e7e7dc2ce773478b4618ed4
Piyush Dangre <piyushdangre11@gmail.com>
2020-05-30 21:21:05 +0530

    Terminal operator examples started

---
+++ src/streamsapi/_3TerminalStreamOps.java
@@ -0,0 +1,11 @@
+package streamsapi;
+
+/**
+ * Since Stream operations are lazy, the iteration is done only
+ * when terminal operator is present.
+ * @author Piyush Dangre
+ *
+ */
+public class _3TerminalStreamOps {
+
+}
